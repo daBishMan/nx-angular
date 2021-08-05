b4ebcdb53cf1ce9a044283a356071ff3a7722b3e
dabishman <jaybishtawi@outlook.com>
2021-08-04 21:11:42 -0600

    Adjust formatting

--- apps/client-app/src/app/app.component.ts
+++ apps/client-app/src/app/app.component.ts
@@ -3,11 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import { Message } from '@nx-angular/api-interfaces';
 
 @Component({
-  selector: 'nx-angular-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'nx-angular-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
+    hello$ = this.http.get<Message>('/api/hello');
+    constructor(private http: HttpClient) {}
 }
