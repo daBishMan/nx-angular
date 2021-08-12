6df97be8c0b4f60a03840ff5fdc992e76d4d06b0
Jay Bishtawi <jaybishtawi@outlook.com>
2021-08-12 16:24:54 -0600

    update

--- apps/client-app/src/app/app.module.ts
+++ apps/client-app/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
     declarations: [AppComponent],
     imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
-    providers: [],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
