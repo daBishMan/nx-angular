9e81f396de3159aa3a0f864e18913a99cd5f0f44
Jay Bishtawi <jaybishtawi@outlook.com>
2021-08-12 16:19:00 -0600

    update

--- apps/client-app/src/app/app.component.ts
+++ apps/client-app/src/app/app.component.ts
@@ -5,9 +5,10 @@ import { Message } from '@nx-angular/api-interfaces';
 @Component({
     selector: 'nx-angular-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+    // hello$ = this.http.get<Message>('/api/hello');
     hello$ = this.http.get('/api/products');
     // xxx = this.http.get('/api/customers');
     constructor(private http: HttpClient) {}
